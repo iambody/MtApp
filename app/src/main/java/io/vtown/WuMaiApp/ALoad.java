8192f97755a3adadd1490a19c2c9c8f0380e186c
datutu <beist@sohu.com>
2017-01-10 15:00:30 +0800

    霾太 第一次提交项目

---
+++ app/src/main/java/io/vtown/WuMaiApp/ALoad.java
@@ -0,0 +1,13 @@
+package io.vtown.WuMaiApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ALoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aload);
+    }
+}
